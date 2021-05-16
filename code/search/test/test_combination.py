f10ead92ada6236547405d737e6225edae44d8e5
Achref-Mekni <61314172+Achref-Mekni@users.noreply.github.com>
2021-05-16 17:46:48 +0200

    Update test_combination.py

--- code/search/test/test_combination.py
+++ code/search/test/test_combination.py
@@ -1,11 +1,42 @@
 #FUNCTIONAL TESTING PART:
 
+#path has to be changed if you would like to run the test depending on your local location of the files 
 sys.path.insert(1, 'E:/Achref/IVT hw/iet-hf2021-regression/code')
+#importing sort library
 import sorting.src.insertion_sort.insertion_sort as insert
 import sorting.src.heap_sort.heap_sort as heap
 import sorting.src.merge_sort.merge_sort as merge
 import sorting.src.circle_sort.circle_sort as circle
 import sorting.src.bubble_sort.bubble_sort as bubble
+#importing search library
+import search.src.linear_search.linear_search as lin
+import search.src.binary_search.binary_search as binr
+import search.src.fibonacci_search.fibonacci_search as fib
+import search.src.ternary_search.ternary_search as ter
+import search.src.jump_search.jump_search as jump
+import search.src.interpolation_search.interpolation_search as inter
+import search.src.exponential_search.exponential_search as exp
+
+def linear_search(mylist, find):
+    return lin.linear_search(mylist,find)
+
+def binary_search_recursive(mylist, find):
+    return binr.binary_search_recursive(mylist,find)
+
+def exponential_search(mylist, find):
+    return exp.exponential_search(mylist, find)
+
+def fibonacci_search(mylist, find):
+    return fib.fibonacci_search(mylist, find)
+
+def interpolation_search(mylist, find):
+    return inter.interpolation_search(mylist, find)
+
+def jump_search(mylist, find):
+    return jump.jump_search(mylist, find)
+
+def ternary_search(mylist, find):
+    return ter.ternary_search(mylist, find)
 
 insertionlist = [59,48,150,10,0,4,6,99,1547,2456,3145]
 mergelist = [59,48,150,10,0,4,6,99,1547,2456,3145]
