01c1ce8d18548e13d2f2b5d0ffd5d3dfa64dfbe6
khosoobatsaikhan <37008087+khosoobatsaikhan@users.noreply.github.com>
2021-05-16 07:25:31 +0200

    Manual code review is done on some searches and sorting

--- code/search/src/interpolation_search/interpolation_search.java
+++ code/search/src/interpolation_search/interpolation_search.java
@@ -1,45 +1,38 @@
 
-
 public class Interpolation {
 
-	    static int arr[] = new int[]{10, 12, 13, 15, 17, 19, 20, 21, 22, 23,
-	                                         24, 33, 35, 42, 49};
-	     
-	public  static int interpolationSearch(int x)
-	    {
-	        // Find indexes of two corners
-	        int lo = 0, hi = (arr.length - 1);
-	      
-	        // Since array is sorted, an element present
-	        // in array must be in range defined by corner
-	        while (lo <= hi && x >= arr[lo] && x <= arr[hi])
-	        {
-	            int pos = lo + (((hi-lo) /
-	                  (arr[hi]-arr[lo]))*(x - arr[lo]));
-	      
-	            if (arr[pos] == x)
-	                return pos;
-	      
-	            if (arr[pos] < x)
-	                lo = pos + 1;
-	      
-	            else
-	                hi = pos - 1;
-	        }
-	        return -1;
-	    }
-	   
-	    public static void main(String[] args) 
-	    {
-	         int x = 20; 
-	         int index = interpolationSearch(x);
-	          
-	         // If element was found
-	         if (index != -1)
-	            System.out.println("Element found at index " + index);
-	         else
-	            System.out.println("Element not found.");
-	    }
+	static int[] arr = new int[] { 10, 12, 13, 15, 17, 19, 20, 21, 22, 23, 24, 33, 35, 42, 49 };
+
+	public static int interpolationSearch(int x) {
+		// Find indexes of two corners
+		int low = 0;
+		int high = (arr.length - 1);
+
+		// Since array is sorted, an element present
+		// in array must be in range defined by corner
+		while (low <= high && x >= arr[low] && x <= arr[high]) {
+			int pos = low + (((high - low) / (arr[high] - arr[low])) * (x - arr[low]));
+
+			if (arr[pos] == x)
+				return pos;
+
+			if (arr[pos] < x)
+				low = pos + 1;
+
+			else
+				high = pos - 1;
+		}
+		return -1;
 	}
 
+	public static void main(String[] args) {
+		int x = 20;
+		int index = interpolationSearch(x);
 
+		// If element was found
+		if (index != -1)
+			Logger.log("Element found at index " + index);
+		else
+			Logger.log("Element not found.");
+	}
+}
--- code/search/src/ternary_search/ternary_search.java
+++ code/search/src/ternary_search/ternary_search.java
@@ -1,28 +1,33 @@
-public static void main(String[] args) {
-    //declare a string array with initial size
+package org.opengenus.cosmos.search;
 
-    String[] songs = {"Ace", "Space", "Diamond"};
+class TernarySearch {
 
-    System.out.println("\nTest binary (String):");
-    System.out.println(search(songs, "Ace", 0, 3));
+    public static int search(String[] x, String target, int start, int end) {
 
-}
-
-public static int search(String[] x, String target, int start, int end) {
-
-    if (start < end) {
-        int midpoint1 = start + (end - start) / 3;
-        int midpoint2 = start + 2 * (end - start) / 3;
-        if (target.compareTo(x[midpoint1]) == 0) {
-            return midpoint1;
-        } else if (target.compareTo(x[midpoint2]) == 0) {
-            return midpoint2;
-        } else if (x[midpoint1].compareTo(x[midpoint2]) < 0) {
-            return search(x, target, midpoint1, end);
-        } else {
-            return search(x, target, start, midpoint2);
+        if (start < end) {
+            int midpoint1 = start + (end - start) / 3;
+            int midpoint2 = start + 2 * (end - start) / 3;
+            if (target.compareTo(x[midpoint1]) == 0) {
+                return midpoint1;
+            } else if (target.compareTo(x[midpoint2]) == 0) {
+                return midpoint2;
+            } else if (x[midpoint1].compareTo(x[midpoint2]) < 0) {
+                return search(x, target, midpoint1, end);
+            } else {
+                return search(x, target, start, midpoint2);
+            }
         }
+
+        return -1;
     }
 
-    return -1;
+    public static void main(String[] args) {
+        // declare a string array with initial size
+
+        String[] songs = { "Ace", "Space", "Diamond" };
+
+        Logger.log("\nTest binary (String):");
+        Logger.log(search(songs, "Ace", 0, 3));
+
+    }
 }
